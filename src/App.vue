7d0e6e29e371634088eded478c0a4538d635f14c
Rhuan Emanuel <rhnrod@gmail.com>
2023-10-01 22:29:28 -0300

    complete single file calculator app

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,117 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, ref } from 'vue';
+
+const state = reactive({
+  n1: 0,
+  n2: 0,
+  ref: ref('somar'),
+  result: function () {
+    switch (state.ref) {
+      case 'subtrair':
+        return state.n1 - state.n2
+      case 'multiplicar':
+        return state.n1 * state.n2
+      case 'dividir':
+        return state.n1 / state.n2
+      default:
+        return state.n1 + state.n2
+    } 
+  }
+});
+
+function changeNumber(e) {
+  let num = e.target.id;
+
+  switch (num) {
+    case 'n1':
+      return state.n1 = Number(e.target.value)
+    case 'n2':
+      return state.n2 = Number(e.target.value)
+  }
+}
+
+function checkNumberLength() {
+  let alpha =  Number(state.result().toString().length) >= 12;
+  return alpha;
+}
+
+function checkNumberLength2() {
+  let alpha =  Number(state.result().toString().length) >= 15;
+  return alpha;
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-8 mt-5 mb-5 d-flex showcase justify-content-end align-items-end">
+        <h1 class="showcase__title">Vue calculator</h1>
+        <span :class="{less: checkNumberLength(), lesser: checkNumberLength2()}" class="showcase__number">{{ state.result() }}</span>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="row d-flex justify-content-center">
+      <div class="col-3">
+        <input type="number" id="n1" @change="changeNumber" @keyup="changeNumber" :placeholder="state.n1" class="form-control">
+      </div>
+      <div class="col-3">
+        <input type="number" id="n2" @change="changeNumber" @keyup="changeNumber" :placeholder="state.n2"  class="form-control">
+      </div>
+      <div class="col-2">
+          <select class="form-select" v-model="state.ref">
+            <option value="somar">somar</option>
+            <option value="subtrair">subtrair</option>
+            <option value="multiplicar">multiplicar</option>
+            <option value="dividir">dividir</option>
+          </select>
+        </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  margin: 0 auto;
+}
+.showcase {
+  height: 25vh;
+  background-color: #282828;
+  border-radius: 12px;
+  position: relative;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.showcase__title {
+  position: absolute;
+  font-size: 12px;
+  color: #ff9100;
+  text-transform: uppercase;
+  top: 10%;
+  right: 2%;
+}
+.showcase__number {
+  font-size: 100px;
+  color: #fefefefe;
+  transition: all ease 1s;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.form-control {
+  font-size: 28px;
+  background-color: #ececec;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.form-select {
+  height: 56px;
+  font-size: 22px;
+  font-weight: bold;
+  background-color: rgb(207, 110, 74);
+  color: #fefefefe
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.less {
+  font-size: 82px;
+}
+
+.lesser {
+  font-size: 56px;
 }
 </style>
